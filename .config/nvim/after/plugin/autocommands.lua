175dce19f43e44a7d1350f5f915cc1357d235b84
ricardobossan <ricardobossan@protonmail.com>
2023-09-24 18:44:07 -0300

    Improves some integration with markdown on calcurse files

--- .config/nvim/after/plugin/autocommands.lua
+++ .config/nvim/after/plugin/autocommands.lua
@@ -3,15 +3,22 @@ function ForceMarkdown()
   vim.cmd("set syntax=markdown")
 end
 
-vim.api.nvim_create_autocmd({"BufWinEnter","BufEnter","BufRead","BufNewFile"}, {
-  pattern = { "markdown", "*.md", "~/.local/share/calcurse/notes/*", "/tmp/calcurse-note*" },
+local HOME = os.getenv "HOME"
+local notesCustom = HOME .. "/calcurse/notes/*"
+local notesDefault = HOME .. "/.local/share/calcurse/notes/*"
+local noteBuff = "/tmp/calcurse-note*"
+
+vim.api.nvim_create_autocmd({ "BufWinEnter", "BufEnter", "BufRead", "BufNewFile" }, {
+  pattern = { "markdown", "*.md", noteBuff, notesDefault, notesCustom },
   command = [[lua ForceMarkdown()]]
 })
 
-vim.schedule_wrap(function()
-  vim.api.nvim_command("autocmd BufNewFile,BufRead ~/.local/share/calcurse/notes/* lua ForceMarkdown()")
-  vim.api.nvim_command("autocmd BufNewFile,BufRead /**/*.md lua ForceMarkdown()")
-end)
+--[[ -- TODO: After a while, if it makes no difference, delete:
+    vim.schedule_wrap(function()
+      vim.api.nvim_command("autocmd BufNewFile,BufRead ~/.local/share/calcurse/notes/* lua ForceMarkdown()")
+      vim.api.nvim_command("autocmd BufNewFile,BufRead /**/*.md lua ForceMarkdown()")
+    end)
+--]]
 
 -- Copia para clipboard do sistema
 vim.cmd("set clipboard+=unnamedplus")
