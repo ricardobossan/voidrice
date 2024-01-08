b7acb8fbadc838473241778e81bf84b9788421b0
ricardobossan <ricardobossan@protonmail.com>
2024-01-08 20:41:50 -0300

    Move from packer to lazy.vim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,73 +1,63 @@
-local packer = require("packer")
-
--- Only required if you have packer configured as `opt`
-vim.cmd([[packadd packer.nvim]])
-
-return packer.startup(function(use)
-  -- Packer can manage itself
-  use({
-    "wbthomason/packer.nvim",
-    commit = "1d0cf98a561f7fd654c970c49f917d74fafe1530",
-  })
+local plugins = {
   -- Explorador de arquivos
 
   -- Terminal
-  use({
+  {
     "akinsho/toggleterm.nvim",
     commit = "ed6c92d6708e5208360347f2281de99602061dc8",
     tag = "*",
     config = function()
       require("toggleterm").setup()
     end,
-  })
+  },
 
-  use({
+  {
     "nvim-treesitter/nvim-treesitter",
     commit = "b23856a88e04e1468e1603d6e0900f2372709e22",
     --{ run = ':TSUpdate' }
-  })
+  },
 
-  use({
+  {
     "nvim-telescope/telescope.nvim",
     --tag = '0.1.1',
-    commit = "dce1156ca103b8222e4abbfc63f9c6887abf5ec6",
+    commit = "87e92ea31b2b61d45ad044cf7b2d9b66dad2a618",
 
     -- or                            , branch = '0.1.x',
-    requires = { { "nvim-lua/plenary.nvim" } },
-  })
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+  },
 
-  use({
+  {
     "morhetz/gruvbox",
     commit = "bf2885a95efdad7bd5e4794dd0213917770d79b7",
-    as = "gruvbox",
+    as = "gruvbox"
     --config = function()
     --vim.cmd[[
     --colorscheme gruvbox
     --]]
     --end
-  })
+  },
 
-  use({
+  {
     "mbbill/undotree",
     commit = "1a23ea84bd02c34f50d8e10a8b4bfc89597ffe4e",
-  })
+  },
 
   -- Null-ls
-  use({
+  {
     "jose-elias-alvarez/null-ls.nvim",
     commit = "7b2b28e207a1df4ebb13c7dc0bd83f69b5403d71",
     config = function()
       require("null-ls").setup()
     end,
-    requires = { "nvim-lua/plenary.nvim", commit = "0c31c398261567cda89b66ddffc69d39495f63ae" },
-  })
+    dependencies = { "nvim-lua/plenary.nvim", commit = "0c31c398261567cda89b66ddffc69d39495f63ae" },
+  },
 
   -- LSP
-  use({
+  {
     "VonHeikemen/lsp-zero.nvim",
     --branch = "v1.x",
     commit = "934a3fdf5072b8cb9da16aadd101901cd1541f3e",
-    requires = {
+    dependencies = {
       -- LSP Support
       { "neovim/nvim-lspconfig",             commit = "886ef7bd76b08d86de3bca6ed9373d2e95c31c55" }, -- Required
       { "williamboman/mason.nvim",           commit = "2311d9d883eb709ad9979a726a38c5ce1343b63c" }, -- Optional
@@ -85,28 +75,28 @@ return packer.startup(function(use)
       { "L3MON4D3/LuaSnip",                  commit = "58fbfc627a93281a77f7d161d4ff702e639677b1" }, -- Required
       { "rafamadriz/friendly-snippets",      commit = "25ddcd96540a2ce41d714bd7fea2e7f75fea8ead" }, -- Optional
     },
-  })
+  },
 
-  use({ "OmniSharp/omnisharp-vim", commit = "5ce267481d79d1dc5f7fb2c96ef74e71910b7024" })
+  { "OmniSharp/omnisharp-vim", commit = "5ce267481d79d1dc5f7fb2c96ef74e71910b7024" },
 
   -- Markdown
 
-  use({ "godlygeek/tabular", commit = "339091ac4dd1f17e225fe7d57b48aff55f99b23a" })
-  use({
+  { "godlygeek/tabular", commit = "339091ac4dd1f17e225fe7d57b48aff55f99b23a" },
+  {
     "jakewvincent/mkdnflow.nvim",
     commit = "51cf944514e49662cca8a075b1a8f485c2c39d0f",
     config = function()
-      require("mkdnflow").setup({})
+      require("mkdnflow").setup({} )
     end,
     ft = "markdown",
-  })
+  },
 
-  use({
+  {
     "preservim/vim-markdown",
     commit = "df4be8626e2c5b2a42eb60e1f100fce469b81f7d",
-  })
+  },
 
-  use({
+  {
     "iamcco/markdown-preview.nvim",
     commit = "02cc3874738bc0f86e4b91f09b8a0ac88aef8e96",
     opt = true,
@@ -115,17 +105,18 @@ return packer.startup(function(use)
     end,
     ft = "markdown",
     cmd = { "MarkdownPreview" },
-    requires = {
+    dependencies = {
       { "zhaozg/vim-diagram",   commit = "bd94f79ac6d701caf52a1b5547069c7b93cf6acf" },
       { "aklt/plantuml-syntax", commit = "845abb56dcd3f12afa6eb47684ef5ba3055802b8" },
     },
-  })
+  },
 
   -- auto closing
-  use({ "windwp/nvim-autopairs", commit = "ae5b41ce880a6d850055e262d6dfebd362bb276e" }) -- autoclose parens, brackets, quotes, etc...
-  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })                          -- autoclose tags
+  { "windwp/nvim-autopairs", commit = "ae5b41ce880a6d850055e262d6dfebd362bb276e" }, -- autoclose parens, brackets, quotes, etc...
 
-  use({
+  { "windwp/nvim-ts-autotag", after = "nvim-treesitter" },                          -- autoclose tags
+
+  {
     "nvim-neorg/neorg",
     commit = "9766bef893ec993af9408ea0d44a8f13adbd1e80",
     config = function()
@@ -142,39 +133,39 @@ return packer.startup(function(use)
     end,
     ft = "norg",
     after = "nvim-treesitter",
-    requires = {
+    dependencies = {
       { "nvim-lua/plenary.nvim", commit = "0c31c398261567cda89b66ddffc69d39495f63ae" },
       { "Pocco81/TrueZen.nvim",  commit = "a31109cd423121e1154ae0844fb312086428306f" },
     },
     disable = true,
-  })
+  },
 
-  use({
+  {
     "phaazon/mind.nvim",
     commit = "e59c52758c399caceb549c698cfa2d65e6bbb9f9",
-    requires = { "nvim-lua/plenary.nvim", commit = "0c31c398261567cda89b66ddffc69d39495f63ae" },
+    dependencies = { "nvim-lua/plenary.nvim", commit = "0c31c398261567cda89b66ddffc69d39495f63ae" },
     config = function()
       require("mind").setup()
     end,
     disable = true,
-  })
+  },
 
-  use({ "kyazdani42/nvim-web-devicons", commit = "bb6d4fd1e010300510172b173ab5205d37af084f" })
+  --{ "kyazdani42/nvim-web-devicons", commit = "bb6d4fd1e010300510172b173ab5205d37af084f" },
 
-  use({
+  {
     "kyazdani42/nvim-tree.lua",
     commit = "08a0aa1a3b7411ee0a7887c8818528b1558cef96",
-  })
+  },
 
-  use({
+  {
     "akinsho/bufferline.nvim",
     commit = "84b0822b2af478d0b4f7b0f9249ca218855331db",
-    requires = "nvim-tree/nvim-web-devicons",
-  })
+    dependencies = "nvim-tree/nvim-web-devicons",
+  },
 
   -- WhichKey
   -- Lua
-  use({
+  {
     "folke/which-key.nvim",
     commit = "5224c261825263f46f6771f1b644cae33cd06995",
     config = function()
@@ -186,37 +177,37 @@ return packer.startup(function(use)
       -- refer to the configuration section below
       --}
     end,
-  })
+  },
   -- Lua
-  use({
+  {
     "folke/trouble.nvim",
     commit = "67337644e38144b444d026b0df2dc5fa0038930f",
-    requires = "nvim-tree/nvim-web-devicons",
+    dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
       require("trouble").setup({
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-      })
+      } )
     end,
-  })
+  },
 
-  use({
+  {
     "nvim-lua/plenary.nvim",
     commit = "253d34830709d690f013daf2853a9d21ad7accab",
-  })
+  },
 
   -- Comments
-  use({
+  {
     "numToStr/Comment.nvim",
     commit = "0236521ea582747b58869cb72f70ccfa967d2e89"
-  })
+  },
 
   -- Todo highlight
-  use({
+  {
     "folke/todo-comments.nvim",
     commit = "077c59586d9d0726b0696dc5680eb863f4e04bc5",
-    requires = "nvim-lua/plenary.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
     config = function()
       require("config.todocomments").setup({
         -- your configuration comes here
@@ -224,9 +215,9 @@ return packer.startup(function(use)
         -- refer to the configuration section below
       })
     end,
-  })
+  },
 
-  use({
+  {
     "mfussenegger/nvim-dap",
     commit = "5b986edc95cf7b37da1db91a5c149daa3ac008d2",
     config = function()
@@ -237,7 +228,7 @@ return packer.startup(function(use)
     keys = { [[<leader>d]] },
     module = { "dap" },
     wants = { "nvim-dap-virtual-text", "nvim-dap-ui", "nvim-dap-python", "which-key.nvim" },
-    requires = {
+    dependencies = {
       -- "alpha2phi/DAPInstall.nvim",
       -- { "Pocco81/dap-buddy.nvim", branch = "dev" },
       { "theHamsta/nvim-dap-virtual-text",   commit = "2971ce3e89b1711cc26e27f73d3f854b559a77d4" },
@@ -251,56 +242,55 @@ return packer.startup(function(use)
         module = "osv",
       },
     },
-  })
+  },
 
-  use({
+  {
     "rcarriga/nvim-dap-ui",
     commit = "bdb94e3853d11b5ce98ec182e5a3719d5c0ef6fd",
-  })
+  },
 
-  use({
+  {
     "aklt/plantuml-syntax",
     commit = "845abb56dcd3f12afa6eb47684ef5ba3055802b8",
-  })
+  },
 
-  use({
+  {
     "vimwiki/vimwiki",
     commit = "c5b7f7e76f7d9b07036948a2f9c1d6cd0167e884"
-
-  })
+  },
   -- ChatGPT
   --[[
   local home = vim.fn.expand("$HOME")
-  use({
+  {
     "jackMort/ChatGPT.nvim",
     config = function()
       require("chatgpt").setup({
         api_key_cmd = "gpg --decrypt " .. home .. "/.local/share/password-store/_pro_tools/chatgpt.gpg",
-      })
+      }
     end,
-    requires = {
+    dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
     },
-  })
+  }
 -]]
   -- FIX: Não faz view para diagramas de classe
-  use({
+  {
     "weirongxu/plantuml-previewer.vim",
     commit = "c0a9e42ad004b706ed3a097c839932110e75aae4",
-  })
+  },
 
-  use({
+  {
     "tyru/open-browser.vim",
     commit = "7d4c1d8198e889d513a030b5a83faa07606bac27",
-  })
-
+  },
+}
   --[[
   use {
     "moll/vim-bbye",
     commit = "25ef93ac5a87526111f43e5110675032dbcacf56"
-  }
+  },
 --]]
   --
   -- Snapshots
@@ -311,4 +301,18 @@ return packer.startup(function(use)
   packer.set_default(default_snapshot)
   packer.dir.snapshots = vim.fn.expand("~/.config/nvim/packer/snapshots")
   --]]
-end)
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(plugins)
