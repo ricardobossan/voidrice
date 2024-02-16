e620a6c9fe5b12ae07f85455db074ecec77ed20a
ricardobossan <ricardobossan@protonmail.com>
2024-02-16 16:20:16 -0300

    Add whichkey bindings and new workspace for obsidian

--- .config/nvim/lua/config/obsidian.lua
+++ .config/nvim/lua/config/obsidian.lua
@@ -1,27 +1,25 @@
 local M = {}
 
 function M.setup()
-  require("obsidian").setup(
-    {
-      workspaces = {
-        {
-          name = "Zettelkasten",
-          path = "~/zettelkasten/Zettelkasten/"
-        },
-        {
-          name = "bys",
-          path = "~/projetos/bys/"
-        }
-      },
-  templates = {
-    subdir = "templates",
-    date_format = "%Y-%m-%d",
-    time_format = "%H:%M:%S",
-    -- A map for custom variables, the key should be the variable and the value a function
-    substitutions = {},
-  },
-    }
-  )
+	require("obsidian").setup({
+		workspaces = {
+			{
+				name = "Zettelkasten",
+				path = "~/zettelkasten/Zettelkasten/",
+			},
+			{
+				name = "bys",
+				path = "~/projetos/bys/",
+			},
+		},
+		templates = {
+			subdir = "templates",
+			date_format = "%Y-%m-%d",
+			time_format = "%H:%M:%S",
+			-- A map for custom variables, the key should be the variable and the value a function
+			substitutions = {},
+		},
+	})
 end
 
 return M
