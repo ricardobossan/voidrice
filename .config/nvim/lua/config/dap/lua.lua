b3ac4659511cea8b2a9f9c1c634016521d53770b
ricardobossan <ricardobossan@protonmail.com>
2024-04-28 23:48:04 -0300

    Gets dap working for lua

--- .config/nvim/lua/config/dap/lua.lua
+++ .config/nvim/lua/config/dap/lua.lua
@@ -2,27 +2,47 @@ local M = {}
 
 
 function M.setup()
-  local dap = require("dap")
-  dap.adapters["local-lua"] = {
-    type = "executable",
-    command = "node",
-    args = {
-      "/home/ricar/.install/local-lua-debugger-vscode/extension/debugAdapter.ts",
-    },
-    enrich_config = function(config, on_config)
-      if not config["extensionPath"] then
-        local c = vim.deepcopy(config)
-        -- 💀 If this is missing or wrong you'll see
-        -- "module 'lldebugger' not found" errors in the dap-repl when trying to launch a debug session
-        c.extensionPath = "/home/ricar/.install/local-lua-debugger-vscode/"
-        on_config(c)
-      else
-        on_config(config)
-      end
-    end,
-  }
+	local dap = require("dap")
+	dap.adapters["local-lua"] = {
+		type = "executable",
+		command = "node",
+		args = {
+			"/home/ricar/.local/share/nvim/local-lua-debugger-vscode/extension/debugAdapter.js",
+		},
+		enrich_config = function(config, on_config)
+			if not config["extensionPath"] then
+				local c = vim.deepcopy(config)
+				-- 💀 If this is missing or wrong you'll see
+				-- "module 'lldebugger' not found" errors in the dap-repl when trying to launch a debug session
+				c.extensionPath = "/home/ricar/.local/share/nvim/local-lua-debugger-vscode/"
+				on_config(c)
+			else
+				on_config(config)
+			end
+		end,
+	}
 
-  dap.configurations.lua = { {} }
+	dap.configurations.lua = {
+		{
+			name = 'Launch',
+			type = 'local-lua',
+			request = 'launch',
+			cwd = '${workspaceFolder}',
+			program = {
+				lua = 'lua',
+				file = '${file}',
+			},
+			args = {},
+		},
+		{
+			name = "Attach",
+			type = "local-lua",
+			request = "attach",
+			program = "${file}",
+			cwd = vim.fn.getcwd(),
+			sourceMaps = true,
+		},
+	}
 end
 
 return M
