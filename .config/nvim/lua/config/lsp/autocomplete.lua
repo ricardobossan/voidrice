9c42fb352a80f80eeeba91a984aec37d62d07e36
ricardobossan <ricardobossan@protonmail.com>
2024-08-05 22:23:16 -0300

    fixes autocomplete.lua indentation

--- .config/nvim/lua/config/lsp/autocomplete.lua
+++ .config/nvim/lua/config/lsp/autocomplete.lua
@@ -1,54 +1,55 @@
 local cmp = require("cmp")
 cmp.setup({
-  completion = {
-    autocomplete = {
-      require('cmp.types').cmp.TriggerEvent.InsertEnter,
-      require('cmp.types').cmp.TriggerEvent.TextChanged
-    }
-  },
-  sources = {
-    -- Copilot
-    --{ name = "copilot" },
-    --{ name = "codeium" },
-    -- Snippets
-    { name = "luasnip" },
-    { name = "neosnippet" },
-    { name = "snippy" },
-    { name = "ultisnips" },
-    { name = "vsnip" },
-    { name = "tsnip" },
+	completion = {
+		autocomplete = {
+			require('cmp.types').cmp.TriggerEvent.TextChanged,
+			require('cmp.types').cmp.TriggerEvent.InsertEnter
+		}
+	},
+	sources = {
+		-- AI
+		--{ name = "copilot" },
+		{ name = "codeium" },
 
-    -- LSP
-    { name = "nvim_lsp" },
-    { name = "nvim_lsp_signature_help" },
-    { name = "nvim_lsp_document_symbol" },
-    { name = "nvim_lsp_signature_help" },
-    { name = "vim_lsp" },
-    { name = "diag-codes" },
+		-- LSP
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "nvim_lsp_document_symbol" },
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "nvim_lsp" },
+		{ name = "vim_lsp" },
+		{ name = "diag-codes" },
 
-    -- Generic
-    { name = "path" },
-    { name = "buffer" },
-    { name = "emoji" },
-  },
-  formatting = {
-    format = require('lspkind').cmp_format({
-      mode = "symbol",
-      maxwidth = 50,
-      ellipsis_char = '...',
-      symbol_map = { Codeium = "", }
-    })
-  },
-  mapping = {
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  },
-  experimental = {
-    ghost_text = true,
-  },
+		-- Snippets
+		{ name = "luasnip" },
+		{ name = "neosnippet" },
+		{ name = "snippy" },
+		{ name = "ultisnips" },
+		{ name = "vsnip" },
+		{ name = "tsnip" },
+
+		-- Generic
+		{ name = "path" },
+		{ name = "buffer" },
+		{ name = "emoji" },
+	},
+	formatting = {
+		format = require('lspkind').cmp_format({
+			mode = "symbol",
+			maxwidth = 50,
+			ellipsis_char = '...',
+			symbol_map = { Codeium = "", }
+		})
+	},
+	mapping = {
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	},
+	experimental = {
+		ghost_text = true,
+	},
 })
 
 -- autocomplete setup
@@ -57,106 +58,106 @@ cmp.setup({
 -- tried to do nvim correspondent to `autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })`
 
 vim.api.nvim_create_autocmd(
-  { "BufWinEnter", "BufEnter", "BufRead", "BufNewFile" },
-  {
-    pattern = { "sql", "mysql", "plsql" },
-    command = [[lua require('cmp').setup.buffer({sources = {{ name = 'vim-dadbod-completion'}} })]],
-  }
+	{ "BufWinEnter", "BufEnter", "BufRead", "BufNewFile" },
+	{
+		pattern = { "sql", "mysql", "plsql" },
+		command = [[lua require('cmp').setup.buffer({sources = {{ name = 'vim-dadbod-completion'}} })]],
+	}
 )
 
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 cmp.event:on("menu_opened", function()
-  vim.b.copilot_suggestion_hidden = true
+	vim.b.copilot_suggestion_hidden = true
 end)
 
 cmp.event:on("menu_closed", function()
-  vim.b.copilot_suggestion_hidden = false
+	vim.b.copilot_suggestion_hidden = false
 end)
 
 local on_attach = function(client, bufnr)
-  -- temporary fix for a roslyn issue in omnisharp
-  -- opened issues:
-  -- https://github.com/OmniSharp/omnisharp-roslyn/issues/2483
-  -- https://github.com/neovim/neovim/issues/21391
-  if client.name == "omnisharp" then
-    client.server_capabilities.semanticTokensProvider = {
-      full = vim.empty_dict(),
-      legend = {
-        tokenModifiers = { "static_symbol" },
-        tokenTypes = {
-          "comment",
-          "excluded_code",
-          "identifier",
-          "keyword",
-          "keyword_control",
-          "number",
-          "operator",
-          "operator_overloaded",
-          "preprocessor_keyword",
-          "string",
-          "whitespace",
-          "text",
-          "static_symbol",
-          "preprocessor_text",
-          "punctuation",
-          "string_verbatim",
-          "string_escape_character",
-          "class_name",
-          "delegate_name",
-          "enum_name",
-          "interface_name",
-          "module_name",
-          "struct_name",
-          "type_parameter_name",
-          "field_name",
-          "enum_member_name",
-          "constant_name",
-          "local_name",
-          "parameter_name",
-          "method_name",
-          "extension_method_name",
-          "property_name",
-          "event_name",
-          "namespace_name",
-          "label_name",
-          "xml_doc_comment_attribute_name",
-          "xml_doc_comment_attribute_quotes",
-          "xml_doc_comment_attribute_value",
-          "xml_doc_comment_cdata_section",
-          "xml_doc_comment_comment",
-          "xml_doc_comment_delimiter",
-          "xml_doc_comment_entity_reference",
-          "xml_doc_comment_name",
-          "xml_doc_comment_processing_instruction",
-          "xml_doc_comment_text",
-          "xml_literal_attribute_name",
-          "xml_literal_attribute_quotes",
-          "xml_literal_attribute_value",
-          "xml_literal_cdata_section",
-          "xml_literal_comment",
-          "xml_literal_delimiter",
-          "xml_literal_embedded_expression",
-          "xml_literal_entity_reference",
-          "xml_literal_name",
-          "xml_literal_processing_instruction",
-          "xml_literal_text",
-          "regex_comment",
-          "regex_character_class",
-          "regex_anchor",
-          "regex_quantifier",
-          "regex_grouping",
-          "regex_alternation",
-          "regex_text",
-          "regex_self_escaped_character",
-          "regex_other_escape",
-        },
-      },
-      range = true,
-    }
-  end
+	-- temporary fix for a roslyn issue in omnisharp
+	-- opened issues:
+	-- https://github.com/OmniSharp/omnisharp-roslyn/issues/2483
+	-- https://github.com/neovim/neovim/issues/21391
+	if client.name == "omnisharp" then
+		client.server_capabilities.semanticTokensProvider = {
+			full = vim.empty_dict(),
+			legend = {
+				tokenModifiers = { "static_symbol" },
+				tokenTypes = {
+					"comment",
+					"excluded_code",
+					"identifier",
+					"keyword",
+					"keyword_control",
+					"number",
+					"operator",
+					"operator_overloaded",
+					"preprocessor_keyword",
+					"string",
+					"whitespace",
+					"text",
+					"static_symbol",
+					"preprocessor_text",
+					"punctuation",
+					"string_verbatim",
+					"string_escape_character",
+					"class_name",
+					"delegate_name",
+					"enum_name",
+					"interface_name",
+					"module_name",
+					"struct_name",
+					"type_parameter_name",
+					"field_name",
+					"enum_member_name",
+					"constant_name",
+					"local_name",
+					"parameter_name",
+					"method_name",
+					"extension_method_name",
+					"property_name",
+					"event_name",
+					"namespace_name",
+					"label_name",
+					"xml_doc_comment_attribute_name",
+					"xml_doc_comment_attribute_quotes",
+					"xml_doc_comment_attribute_value",
+					"xml_doc_comment_cdata_section",
+					"xml_doc_comment_comment",
+					"xml_doc_comment_delimiter",
+					"xml_doc_comment_entity_reference",
+					"xml_doc_comment_name",
+					"xml_doc_comment_processing_instruction",
+					"xml_doc_comment_text",
+					"xml_literal_attribute_name",
+					"xml_literal_attribute_quotes",
+					"xml_literal_attribute_value",
+					"xml_literal_cdata_section",
+					"xml_literal_comment",
+					"xml_literal_delimiter",
+					"xml_literal_embedded_expression",
+					"xml_literal_entity_reference",
+					"xml_literal_name",
+					"xml_literal_processing_instruction",
+					"xml_literal_text",
+					"regex_comment",
+					"regex_character_class",
+					"regex_anchor",
+					"regex_quantifier",
+					"regex_grouping",
+					"regex_alternation",
+					"regex_text",
+					"regex_self_escaped_character",
+					"regex_other_escape",
+				},
+			},
+			range = true,
+		}
+	end
 
-  -- specifies what to do when language server attaches to the buffer
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	-- specifies what to do when language server attaches to the buffer
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 end
